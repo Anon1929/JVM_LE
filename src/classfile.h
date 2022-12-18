9f72b6dc308a3d9cccbb437316c3a4a4db995313
mcr9n <artsoucor12@gmail.com>
2022-12-18 11:34:18 -0300

    criando structs do classfile

--- src/classfile.h
+++ src/classfile.h
@@ -29,18 +29,74 @@ typedef unsigned int u4;
 typedef unsigned long u8;
 
 typedef struct cp_info{
+    u1 tag;
+    union{
+        struct{
+            u2 name_index;
+        }class;
+        struct{
+            u2 class_index;
+            u2 name_and_type_index;
+        }field_ref;
+        struct{
+            u2 class_index;
+            u2 name_and_type_index;
 
+        }method_ref;
+        struct{
+            u2 class_index;
+            u2 name_and_type_index;
+        }interface_method_ref;
+        struct{
+            u2 name_index;
+            u2 descriptor_index;
+        }name_and_type;
+        struct {
+            u2 length;
+            u1 *bytes;
+        }utf_8_info;
+        struct{
+            u2 string_index;
+        }string_info;
+        struct{
+            u4 bytes;
+        }integer_info;
+        struct{
+            u4 bytes;
+        }float_info;
+        struct{
+            u4 high_bytes;
+            u4 low_bytes;
+        }long_info;
+        struct{
+            u4 high_bytes;
+            u4 low_bytes;
+        }double_info;
+
+    }cp_info_union;
 }cp_info;
 
 typedef struct field_info{
-
+    u2 access_flags;
+    u2 name_index;
+    u2 descriptor_index;
+    u2 attributes_count;
+    attribute_info *attributes;
 }field_info;
 
 typedef struct method_info{
+    u2 access_flags;
+    u2 name_index;
+    u2 descriptor_index;
+    u2 attributes_count;
+    attribute_info *attributes;
 
 }method_info;
 
 typedef struct attribute_info{
+    u2 attribute_name_index;
+    u4 attribute_length;
+    u1 *info;
 
 }attribute_info;
 
