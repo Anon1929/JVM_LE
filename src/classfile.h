06c5b320af156c44b91c715674a7007fca9b377d
mcr9n <artsoucor12@gmail.com>
2022-12-22 10:12:44 -0300

    lendo tipos mais simples, arquivo de teste, e tentativa de ler cp_info

--- src/classfile.h
+++ src/classfile.h
@@ -28,96 +28,111 @@ typedef unsigned short u2;
 typedef unsigned int u4;
 typedef unsigned long u8;
 
-
-typedef struct attribute_info{
+typedef struct attribute_info
+{
     u2 attribute_name_index;
     u4 attribute_length;
     u1 *info;
 
-}attribute_info;
+} attribute_info;
 
-
-typedef struct cp_info{
+typedef struct cp_info
+{
     u1 tag;
-    union{
-        struct{
+    union
+    {
+        struct
+        {
             u2 name_index;
-        }class;
-        struct{
+        } class_info;
+        struct
+        {
             u2 class_index;
             u2 name_and_type_index;
-        }field_ref;
-        struct{
+        } field_ref;
+        struct
+        {
             u2 class_index;
             u2 name_and_type_index;
 
-        }method_ref;
-        struct{
+        } method_ref;
+        struct
+        {
             u2 class_index;
             u2 name_and_type_index;
-        }interface_method_ref;
-        struct{
+        } interface_method_ref;
+        struct
+        {
             u2 name_index;
             u2 descriptor_index;
-        }name_and_type;
-        struct {
+        } name_and_type;
+        struct
+        {
             u2 length;
             u1 *bytes;
-        }utf_8_info;
-        struct{
+        } utf_8_info;
+        struct
+        {
             u2 string_index;
-        }string_info;
-        struct{
+        } string_info;
+        struct
+        {
             u4 bytes;
-        }integer_info;
-        struct{
+        } integer_info;
+        struct
+        {
             u4 bytes;
-        }float_info;
-        struct{
+        } float_info;
+        struct
+        {
             u4 high_bytes;
             u4 low_bytes;
-        }long_info;
-        struct{
+        } long_info;
+        struct
+        {
             u4 high_bytes;
             u4 low_bytes;
-        }double_info;
+        } double_info;
 
-    }cp_info_union;
-}cp_info;
+    } cp_info_union;
+} cp_info;
 
-typedef struct field_info{
+typedef struct field_info
+{
     u2 access_flags;
     u2 name_index;
     u2 descriptor_index;
     u2 attributes_count;
     attribute_info *attributes;
-}field_info;
-typedef struct method_info{
+} field_info;
+typedef struct method_info
+{
     u2 access_flags;
     u2 name_index;
     u2 descriptor_index;
     u2 attributes_count;
     attribute_info *attributes;
 
-}method_info;
+} method_info;
 
-typedef struct Classfile {
-    u4             magic;
-    u2             minor_version;
-    u2             major_version;
-    u2             constant_pool_count;
-    cp_info        *constant_pool;
-    u2             access_flags;
-    u2             this_class;
-    u2             super_class;
-    u2             interfaces_count;
-    u2             interfaces;
-    u2             fields_count;
-    field_info     *fields;
-    u2             methods_count;
-    method_info    *methods;
-    u2             attributes_count;
+typedef struct Classfile
+{
+    u4 magic;
+    u2 minor_version;
+    u2 major_version;
+    u2 constant_pool_count;
+    cp_info *constant_pool;
+    u2 access_flags;
+    u2 this_class;
+    u2 super_class;
+    u2 interfaces_count;
+    u2 interfaces;
+    u2 fields_count;
+    field_info *fields;
+    u2 methods_count;
+    method_info *methods;
+    u2 attributes_count;
     attribute_info *attributes;
-}Classfile;
+} Classfile;
 
 #endif
--- src/leitor.c
+++ src/leitor.c
@@ -1,1 +1,91 @@
 #include "leitor.h"
+#include <stdio.h>
+#include "classfile.h"
+
+FILE *fd; //como posso abrir a o arquivo .class?
+
+static u1 u1Read(FILE *fd)
+{
+    u1 toReturn = getc(fd);
+    return toReturn;
+}
+static u2 u2Read(FILE *fd)
+{
+    u2 toReturn = getc(fd);
+    toReturn = (toReturn << 8) | (getc(fd));
+    return toReturn;
+}
+static u4 u4Read(FILE *fd)
+{
+    u4 toReturn = getc(fd);
+    toReturn = (toReturn << 8) | (getc(fd));
+    toReturn = (toReturn << 8) | (getc(fd));
+    toReturn = (toReturn << 8) | (getc(fd));
+    return toReturn;
+}
+static u8 u8Read(FILE *fd)
+{
+    u8 toReturn = getc(fd);
+    toReturn = (toReturn << 8) | (getc(fd));
+    toReturn = (toReturn << 8) | (getc(fd));
+    toReturn = (toReturn << 8) | (getc(fd));
+    toReturn = (toReturn << 8) | (getc(fd));
+    toReturn = (toReturn << 8) | (getc(fd));
+    toReturn = (toReturn << 8) | (getc(fd));
+    toReturn = (toReturn << 8) | (getc(fd));
+    return toReturn;
+}
+//lendo cp_info
+// void readCpinfo(cp_info* cp, FILE *fd)
+// {
+//     cp->tag = u1Read(fd);
+//     switch (cp->tag)
+//     {
+//     case CONSTANT_Class:
+//         cp->class_info.name_index = u2Read(fd);
+//         break;
+//     case CONSTANT_Fieldref:
+//         cp->field_ref.class_index = u2Read(fd);
+//         cp->field_ref.name_and_type_index = u2Read(fd);
+//         break;
+//     case CONSTANT_Methodref:
+//         cp->method_ref.class_index = u2Read(fd);
+//         cp->method_ref.name_and_type_index = u2Read(fd);
+//         break;
+//     case CONSTANT_InterfaceMethodref:
+//         cp->interface_method_ref.class_index = u2Read(fd);
+//         cp->interface_method_ref.name_and_type_index = u2Read(fd);
+//         break;
+//     case CONSTANT_String:
+//         cp->string_info.string_index = u2Read(fd);
+//         break;
+//     case CONSTANT_Integer:
+//         cp->integer_info.bytes = u4Read(fd);
+//         break;
+//     case CONSTANT_Float:
+//         cp->float_info.bytes = u4Read(fd);
+//         break;
+//     case CONSTANT_Long:
+//         cp->long_info.high_bytes = u4Read(fd);
+//         cp->long_info.low_bytes = u4Read(fd);
+//         break;
+//     case CONSTANT_Double:
+//         cp->double_info.high_bytes = u4Read(fd);
+//         cp->double_info.low_bytes = u4Read(fd);
+//         break;
+//     case CONSTANT_NameAndType:
+//         cp->name_and_type.name_index = u2Read(fd);
+//         cp->name_and_type.descriptor_index = u2Read(fd);
+//         break;
+//     case CONSTANT_Utf8:
+//         cp->utf_8_info.length = u2Read(fd);
+//         cp->utf_8_info.bytes = (u1 *)malloc(cp->utf_8_info.length * sizeof(u1));
+//         for (int i = 0; i < cp->utf_8_info.length; i++)
+//         {
+//             cp->utf_8_info.bytes[i] = u1Read(fd);
+//         }
+//         break;
+//     default:
+//         break;
+//     }
+// }
