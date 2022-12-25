5ed0bc3ceac6844ad4a1956cff4e8b2f8da62a53
mcr9n <artsoucor12@gmail.com>
2022-12-25 16:22:59 -0300

    adicionando enumeração de tags e suposto leitor de cp_info

--- src/leitor.c
+++ src/leitor.c
@@ -2,8 +2,13 @@
 #include <stdio.h>
 #include "classfile.h"
 
-FILE *fd; //como posso abrir a o arquivo .class?
-
+//abrindo arquivo .class
+static FILE *openFile(char *path)
+{
+    FILE *fd = fopen(path, "rb");
+    return fd;
+}
+//lendo u1, u2, u4 e u8
 static u1 u1Read(FILE *fd)
 {
     u1 toReturn = getc(fd);
@@ -36,56 +41,58 @@ static u8 u8Read(FILE *fd)
     return toReturn;
 }
 //lendo cp_info
-// void readCpinfo(cp_info* cp, FILE *fd)
-// {
-//     cp->tag = u1Read(fd);
-//     switch (cp->tag)
-//     {
-//     case CONSTANT_Class:
-//         cp->class_info.name_index = u2Read(fd);
-//         break;
-//     case CONSTANT_Fieldref:
-//         cp->field_ref.class_index = u2Read(fd);
-//         cp->field_ref.name_and_type_index = u2Read(fd);
-//         break;
-//     case CONSTANT_Methodref:
-//         cp->method_ref.class_index = u2Read(fd);
-//         cp->method_ref.name_and_type_index = u2Read(fd);
-//         break;
-//     case CONSTANT_InterfaceMethodref:
-//         cp->interface_method_ref.class_index = u2Read(fd);
-//         cp->interface_method_ref.name_and_type_index = u2Read(fd);
-//         break;
-//     case CONSTANT_String:
-//         cp->string_info.string_index = u2Read(fd);
-//         break;
-//     case CONSTANT_Integer:
-//         cp->integer_info.bytes = u4Read(fd);
-//         break;
-//     case CONSTANT_Float:
-//         cp->float_info.bytes = u4Read(fd);
-//         break;
-//     case CONSTANT_Long:
-//         cp->long_info.high_bytes = u4Read(fd);
-//         cp->long_info.low_bytes = u4Read(fd);
-//         break;
-//     case CONSTANT_Double:
-//         cp->double_info.high_bytes = u4Read(fd);
-//         cp->double_info.low_bytes = u4Read(fd);
-//         break;
-//     case CONSTANT_NameAndType:
-//         cp->name_and_type.name_index = u2Read(fd);
-//         cp->name_and_type.descriptor_index = u2Read(fd);
-//         break;
-//     case CONSTANT_Utf8:
-//         cp->utf_8_info.length = u2Read(fd);
-//         cp->utf_8_info.bytes = (u1 *)malloc(cp->utf_8_info.length * sizeof(u1));
-//         for (int i = 0; i < cp->utf_8_info.length; i++)
-//         {
-//             cp->utf_8_info.bytes[i] = u1Read(fd);
-//         }
-//         break;
-//     default:
-//         break;
-//     }
-// }
+void readCpinfo(cp_info* cp, FILE *fd)
+{
+    cp->tag = u1Read(fd);
+    switch (cp->tag)
+    {
+    case CONSTANT_Class:
+        cp->cp_info_union.class_info.name_index = u2Read(fd);
+        break;
+    case CONSTANT_Fieldref:
+        cp->cp_info_union.field_ref.class_index = u2Read(fd);
+        cp->cp_info_union.field_ref.name_and_type_index = u2Read(fd);
+        break;
+    case CONSTANT_Methodref:
+        cp->cp_info_union.method_ref.class_index = u2Read(fd);
+        cp->cp_info_union.method_ref.name_and_type_index = u2Read(fd);
+        break;
+    case CONSTANT_InterfaceMethodref:
+        cp->cp_info_union.interface_method_ref.class_index = u2Read(fd);
+        cp->cp_info_union.interface_method_ref.name_and_type_index = u2Read(fd);
+        break;
+    case CONSTANT_String:
+        cp->cp_info_union.string_info.string_index = u2Read(fd);
+        break;
+    case CONSTANT_Integer:
+        cp->cp_info_union.integer_info.bytes = u4Read(fd);
+        break;
+    case CONSTANT_Float:
+        cp->cp_info_union.float_info.bytes = u4Read(fd);
+        break;
+    case CONSTANT_Long:
+        cp->cp_info_union.long_info.high_bytes = u4Read(fd);
+        cp->cp_info_union.long_info.low_bytes = u4Read(fd);
+        break;
+    case CONSTANT_Double:
+        cp->cp_info_union.double_info.high_bytes = u4Read(fd);
+        cp->cp_info_union.double_info.low_bytes = u4Read(fd);
+        break;
+    case CONSTANT_NameAndType:
+        cp->cp_info_union.name_and_type.name_index = u2Read(fd);
+        cp->cp_info_union.name_and_type.descriptor_index = u2Read(fd);
+        break;
+    case CONSTANT_Utf8:
+        cp->cp_info_union.utf_8_info.length = u2Read(fd);
+        cp->cp_info_union.utf_8_info.bytes = (u1 *)malloc(cp->cp_info_union.utf_8_info.length * sizeof(u1));
+        for (int i = 0; i < cp->cp_info_union.utf_8_info.length; i++)
+        {
+            cp->cp_info_union.utf_8_info.bytes[i] = u1Read(fd);
+        }
+        break;
+    default:
+        break;
+    }
+    
+        
+}
